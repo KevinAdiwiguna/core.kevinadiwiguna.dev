195afe565d54a54fe9d90b616975a552d6e10a52
KevinAdiwiguna <f1d02410115@student.unram.ac.id>
2026-07-25 12:33:52 +0800

    change env var

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,7 +7,7 @@ export const auth = betterAuth({
         provider: "postgresql",
     }),
     secret: process.env.BETTER_AUTH_SECRET,
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
     emailAndPassword: {
         enabled: true,
     },
